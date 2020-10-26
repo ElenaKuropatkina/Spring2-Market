526be90b702321396a0d8f55f156eb76043ec86f
ElenaKuropatkina <el-vita81@yandex.ru>
2020-10-26 18:32:46 +0300

    init

---
+++ admin-ui/src/test/java/com/elenakuropatkina/adminui/AdminUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elenakuropatkina.adminui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ shop-ui/src/test/java/com/elenakuropatkina/shopui/ShopUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elenakuropatkina.shopui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
